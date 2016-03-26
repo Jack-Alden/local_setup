4e0bfb2729c0f4cfe59a0952111e5e333c6269d3
Jack-Alden <sirnapier88@gmail.com>
2016-03-26 14:34:55 -0500

    Updater now adds files if not present, uses root_modifier as argument

--- setup_updater/update_local_setup.rb
+++ setup_updater/update_local_setup.rb
@@ -2,30 +2,41 @@ require_relative 'configs_list.rb'
 
 configs_list = [
   ".bash_profile",
-  ".gitconfig"
+  ".gitconfig",
+  ["Library/Application Support/KomodoEdit/9.3/schemes/", "Alden_Code.ksf"]
 ]
 
 updates    = []
+added      = []
 no_changes = []
 
 configs_list.each do |f|
-  f = FileToCheck.new(f)
+  f = FileToCheck.new(*f)
   
-  unless f.updated?
-    FileUtils.cp(f.edit_path, f.backup_path)
+  if f.need_to_add?
+    FileUtils.cp(f.edit_path, f.backup_add_path)      # 1
+    added.push(f.file_name)
+  elsif f.updated?
+    FileUtils.cp(f.edit_path, f.backup_path)      # 1
     updates.push(f.file_name)
   else
     no_changes.push(f.file_name)
   end
 end
 
-if updates.empty?
+if updates.empty? && added.empty?
   puts "Nothing to update!".green
 else
-  puts "Updated the following files in your backup directory".green
-  updates.each { |item| puts ("    " + item).yellow }
+  unless added.empty?
+    puts "Added the following files to your backup directory".green
+    added.each { |item| puts ("    " + item).yellow }               # 2
+  end
+  unless updates.empty?
+    puts "Updated the following files in your backup directory".green
+    updates.each { |item| puts ("    " + item).yellow }             # 2
+  end
   unless no_changes.empty?
     puts "No changes made to the following files".green
-    no_changes.each { |item| puts ("    " + item).yellow }
+    no_changes.each { |item| puts ("    " + item).yellow }          # 2
   end
 end
--- setup_updater/updater_classes.rb
+++ setup_updater/updater_classes.rb
@@ -3,19 +3,25 @@ require_relative 'colorize.rb'
 
 class FileToCheck
   
-  attr_accessor :file_name
-  attr_reader   :root_dir, :edit_path, :backup_path
+  attr_accessor :root_modifier, :file_name
+  attr_reader   :root_dir, :edit_path, :backup_add_path, :backup_path
   
-  def initialize(file_name)
-    @file_name   = file_name
+  def initialize(root_modifier = "", file_name)
+    @root_modifier   = root_modifier
+    @file_name       = file_name
   # this parameter will get updated when I figure out how to find root path on all machines
-    @root_dir    = "/Users/Jack_Alden/"
-    @edit_path   = @root_dir + @file_name
+    @root_dir        = "/Users/Jack_Alden/"
+    @edit_path       = @root_dir + @root_modifier + @file_name
   # update_path points to where local_setup is kept
-    @backup_path = @root_dir + "Documents/local_setup/" + @file_name
+    @backup_add_path = @root_dir + "Documents/local_setup/"
+    @backup_path     = @backup_add_path + @file_name
+  end
+  
+  def need_to_add?
+    !File.file?(@backup_path)
   end
   
   def updated?
-    FileUtils.identical?(@backup_path, @edit_path)
+    !FileUtils.identical?(@backup_path, @edit_path)
   end
 end
