0cb61baa47f8f313b583a1a5556eeeab94d48cbd
Jack-Barry <sirnapier88@gmail.com>
2016-06-24 17:09:39 -0500

    Make run.rb configurable to different user

--- confignewton/bin/run.rb
+++ confignewton/bin/run.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 require 'grit'
 include Grit
 
-USERNAME = "Jack_Barry"
+USERNAME = "" # Enter your username here
 
 current = File.expand_path('../../', __FILE__)
 Dir["#{current}/lib/**/*.rb"].each { |f| require f }
